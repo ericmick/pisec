77476a47f9438a6e3c4933b68e7bbd95a56653e9
Eric Mickelsen <mickelsen.eric@gmail.com>
2014-05-04 16:31:40 -0700

    Create vid.py
    stream video to socket

---
+++ vid.py
@@ -0,0 +1,27 @@
+import socket
+import time
+import picamera
+
+# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+# all interfaces)
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8000))
+server_socket.listen(0)
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        # Start a preview and let the camera warm up for 2 seconds
+        camera.start_preview()
+        time.sleep(2)
+        # Start recording, sending the output to the connection for 60
+        # seconds, then stop
+        camera.start_recording(connection, format='h264')
+        camera.wait_recording(60)
+        camera.stop_recording()
+finally:
+    connection.close()
+    server_socket.close()
